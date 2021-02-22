eef6533dd934c6050c8f4a51ca0953b2b164bed6
Mehul Mohan <mehulmpt@gmail.com>
2021-02-22 20:22:39 +0530

    solution

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PUBLIC_PORT
+const { newsArticleModel: Model } = require('./connector')
 
 const onePageArticleCount = 10
 
@@ -9,5 +10,14 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+app.get('/newsFeed', async (req, res) => {
+	const limit = parseInt(req.query.limit, 10) || onePageArticleCount
+	const offset = parseInt(req.query.offset, 10) || 0
+
+	const docs = await Model.find().skip(offset).limit(limit).lean()
+
+	res.json(docs)
+})
 
 module.exports = app
